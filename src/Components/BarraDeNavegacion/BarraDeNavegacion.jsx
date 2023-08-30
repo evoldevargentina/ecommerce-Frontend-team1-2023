43f9d0166f255876c5cbe816a07b85a330f243c2
Mateo Lohezic <mateolohezic@gmail.com>
2023-08-29 22:19:21 -0300

    Project Created

---
+++ src/Components/BarraDeNavegacion/BarraDeNavegacion.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './barraDeNavegacion.css'
+
+function BarraDeNavegacion() {
+    return (
+    <>
+        BarraDeNavegacion
+    </>
+    )
+}
+
+export default BarraDeNavegacion
