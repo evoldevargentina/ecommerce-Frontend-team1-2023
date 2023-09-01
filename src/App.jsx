f2802bbbded6f5e774e8ca7b4ccbc688c9c056b8
Mateo Lohezic <mateolohezic@gmail.com>
2023-09-01 19:17:56 -0300

    Update: login style

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,14 @@ import React from 'react'
 import { Route, Routes } from "react-router-dom";
 import Home from './Pages/Home/Home';
 import './App.css'
-import BarraDeNavegacion from './Components/BarraDeNavegacion/BarraDeNavegacion';
-import Login from './Components/login/Login';
+import Login from './Components/Login/Login';
 
 function App() {
 
   return (
     <>
-    <BarraDeNavegacion />
     <Routes>
-      <Route path="/" element={<Home/>} />
+      <Route path='/' element={<Home/>} />
       <Route path='/login' element = {<Login/>}/>
     </Routes>
     </>
